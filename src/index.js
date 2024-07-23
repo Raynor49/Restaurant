b893c6f4e9973c5ced1de47f4e986d0213ff8eb0
Willis <willisroberts49@gmail.com>
2024-07-23 16:41:43 -0400

    restaurant front page exists from both constant html and also the index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log("this is from index.js");
+
+const content = document.getElementById('content');
+
+const restaurantTitle = document.createElement('div');
+restaurantTitle.innerHTML = "RESTAURANT TITLE FROM JS"
+
+content.append(restaurantTitle);
