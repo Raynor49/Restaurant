c067451104f7db1cf90742574a8a4476edde7160
Willis <willisroberts49@gmail.com>
2024-07-23 17:01:23 -0400

    html loading directly from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ console.log("this is from index.js");
 
 const content = document.getElementById('content');
 
-const restaurantTitle = document.createElement('div');
-restaurantTitle.innerHTML = "RESTAURANT TITLE FROM JS"
+const restaurantTitle = document.createElement('h1');
+restaurantTitle.innerHTML = "THIS IS A REAL RESTAURANT WITH REAL FOOD PLEASE EAT"
+
+const foodImage = document.createElement('img');
+foodImage.alt = "";
+foodImage.src = "../src/assets/foodImage.jpg"
 
 content.append(restaurantTitle);
+content.append(foodImage);
